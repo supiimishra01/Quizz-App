6f0f28af56102a9816af88b964d9c4d6b940b27b
SUPRIYA MISHRA <140587863+supiimishra01@users.noreply.github.com>
2024-04-13 00:16:50 +0530

    Add files via upload

---
+++ src/Assets/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+      question:"Who among the following has invented worldwide web?",
+      option1:"Tim Berners-Lee",
+      option2:"George Boole",
+      option3:"Jean Bartik",
+      option4:"Per Brinch Hansen",
+      ans: 1,
+    },
+
+    {
+        question:"Which among the following is the shortcut key to delete a file without moving it into Recycle bin?",
+        option1:"Shift + Delete",
+        option2:"Ctrl + Delete",
+        option3:"Alt + Delete",
+        option4:"Delete + F5",
+        ans: 1,
+      },
+
+      {
+        question:"Which among the following types of memories get refreshed several times per second?",
+        option1:"Static RAM",
+        option2:"Running RAM",
+        option3:"Dynamic RAM",
+        option4:"ROM",
+        ans: 3,
+      },
+
+      {
+        question:"MS-Office is a/an ……",
+        option1:"Programing software",
+        option3:"Operating software",
+        option2:"Option1 & Option2 both",
+        option4:"Application software",
+        ans:4,
+      },
+
+      {
+        question:"The largest unit of measuring computer storage capacity?",
+        option1:"KB",
+        option2:"MB",
+        option3:"TB",
+        option4:"GB",
+        ans: 3,
+      },
+     
+];
